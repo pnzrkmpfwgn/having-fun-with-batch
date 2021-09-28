a99b28ee73eaeeeefba2d2e1ce31acebf1b4ae40
Ayberk Ü. Yiğit <61189367+pnzrkmpfwgn@users.noreply.github.com>
2021-09-28 16:59:12 +0300

    Add files via upload

---
+++ Kesinlikle  v̸̘͒ĩ̵̭r̴̹͋ü̶͚̚ş̸̓ değil.bat
@@ -0,0 +1,469 @@
+@ECHO OFF
+echo İİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİ
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+echo İİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİ
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+echo İİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİ
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+echo İİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİ
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+echo İİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİ
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+echo İİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİ
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+echo İİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİ
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+echo İİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİ
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+echo İİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİ
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+echo İİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİ
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+echo İİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİ
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+echo İİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİ
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+echo İİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİ
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+echo İİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİ
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+echo İİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİ
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+echo İİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİ
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+echo İİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİ
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+echo İİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİ
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+echo İİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİ
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+echo İİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİ
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+echo İİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİ
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+echo İİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİ
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+echo İİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİ
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+echo İİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİ
+PING localhost -n 1 >NUL
+echo İİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİ
+PING localhost -n 1 >NUL
+echo İİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİ
+PING localhost -n 1 >NUL
+echo İİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİ
+PING localhost -n 1 >NUL
+echo İİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİ
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+echo İİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİİ
+title hackleniyor..
+timeout /t 3
+title hacklendin.
+chcp 65001
+echo.
+echo.
+echo.
+echo.
+echo -----------------------------HECKLENDİN, AY Pİ ADRESİN ELİMDE.-------------------------------------------
+echo.
+echo ⡯⡯⡾⠝⠘⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢊⠘⡮⣣⠪⠢⡑⡌
+echo ⡯⡯⡾⠝⠘⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢊⠘⡮⣣⠪⠢⡑⡌
+echo ⠟⠝⠈⠀⠀⠀⠡⠀⠠⢈⠠⢐⢠⢂⢔⣐⢄⡂⢔⠀⡁⢉⠸⢨⢑⠕⡌
+echo ⡀⠁⠀⠀⠀⡀⢂⠡⠈⡔⣕⢮⣳⢯⣿⣻⣟⣯⣯⢷⣫⣆⡂⠀⠀⢐⠑⡌
+echo ⢀⠠⠐⠈⠀⢀⢂⠢⡂⠕⡁⣝⢮⣳⢽⡽⣾⣻⣿⣯⡯⣟⣞⢾⢜⢆⠀⡀⠀⠪
+echo ⣬⠂⠀⠀⢀⢂⢪⠨⢂⠥⣺⡪⣗⢗⣽⢽⡯⣿⣽⣷⢿⡽⡾⡽⣝⢎⠀⠀⠀⢡
+echo ⣿⠀⠀⠀⢂⠢⢂⢥⢱⡹⣪⢞⡵⣻⡪⡯⡯⣟⡾⣿⣻⡽⣯⡻⣪⠧⠑⠀⠁⢐
+echo ⣿⠀⠀⠀⠢⢑⠠⠑⠕⡝⡎⡗⡝⡎⣞⢽⡹⣕⢯⢻⠹⡹⢚⠝⡷⡽⡨⠀⠀⢔
+echo ⣿⡯⠀⢈⠈⢄⠂⠂⠐⠀⠌⠠⢑⠱⡱⡱⡑⢔⠁⠀⡀⠐⠐⠐⡡⡹⣪⠀⠀⢘
+echo ⣿⣽⠀⡀⡊⠀⠐⠨⠈⡁⠂⢈⠠⡱⡽⣷⡑⠁⠠⠑⠀⢉⢇⣤⢘⣪⢽⠀⢌⢎
+echo ⣿⢾⠀⢌⠌⠀⡁⠢⠂⠐⡀⠀⢀⢳⢽⣽⡺⣨⢄⣑⢉⢃⢭⡲⣕⡭⣹⠠⢐⢗
+echo ⣿⡗⠀⠢⠡⡱⡸⣔⢵⢱⢸⠈⠀⡪⣳⣳⢹⢜⡵⣱⢱⡱⣳⡹⣵⣻⢔⢅⢬⡷
+echo ⣷⡇⡂⠡⡑⢕⢕⠕⡑⠡⢂⢊⢐⢕⡝⡮⡧⡳⣝⢴⡐⣁⠃⡫⡒⣕⢏⡮⣷⡟
+echo ⣷⣻⣅⠑⢌⠢⠁⢐⠠⠑⡐⠐⠌⡪⠮⡫⠪⡪⡪⣺⢸⠰⠡⠠⠐⢱⠨⡪⡪⡰
+echo ⣯⢷⣟⣇⡂⡂⡌⡀⠀⠁⡂⠅⠂⠀⡑⡄⢇⠇⢝⡨⡠⡁⢐⠠⢀⢪⡐⡜⡪⡊
+echo ⣿⢽⡾⢹⡄⠕⡅⢇⠂⠑⣴⡬⣬⣬⣆⢮⣦⣷⣵⣷⡗⢃⢮⠱⡸⢰⢱⢸⢨⢌
+echo ⣯⢯⣟⠸⣳⡅⠜⠔⡌⡐⠈⠻⠟⣿⢿⣿⣿⠿⡻⣃⠢⣱⡳⡱⡩⢢⠣⡃⠢⠁
+echo ⡯⣟⣞⡇⡿⣽⡪⡘⡰⠨⢐⢀⠢⢢⢄⢤⣰⠼⡾⢕⢕⡵⣝⠎⢌⢪⠪⡘⡌⠀
+echo ⡯⣳⠯⠚⢊⠡⡂⢂⠨⠊⠔⡑⠬⡸⣘⢬⢪⣪⡺⡼⣕⢯⢞⢕⢝⠎⢻⢼⣀⠀
+echo ⠁⡂⠔⡁⡢⠣⢀⠢⠀⠅⠱⡐⡱⡘⡔⡕⡕⣲⡹⣎⡮⡏⡑⢜⢼⡱⢩⣗⣯⣟
+echo ⢀⢂⢑⠀⡂⡃⠅⠊⢄⢑⠠⠑⢕⢕⢝⢮⢺⢕⢟⢮⢊⢢⢱⢄⠃⣇⣞⢞⣞⢾
+echo ⢀⠢⡑⡀⢂⢊⠠⠁⡂⡐⠀⠅⡈⠪⠪⠪⠣⠫⠑⡁⢔⠕⣜⣜⢦⡰⡎⡯⡾⡽
+
+color 1
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+color 8
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+color 2
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+color 1
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+color 4
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+color 6
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+
+Set /A Sleep+=1
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+color 1
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+color 8
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+color 2
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+color 1
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+color 4
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+color 6
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+echo.
+echo polisi karıştırma yoksa ay pi adresin ölür.
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+echo.
+
+Set Sleep=0  
+:start
+
+if %Sleep% == 40 ( goto end )
+color 1
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+color 8
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+color 2
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+color 1
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+color 4
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+color 6
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+PING localhost -n 1 >NUL
+
+Set /A Sleep+=1
+goto start
+:end
+
+PAUSE
